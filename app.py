a1ca2f173bda031d560934c964aac4ad0c6635c0
[rde-jr03] <[ronnieevangelistajr@gmail.com]>
2024-05-25 22:24:26 +0800

    Added a security

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dicttoxml
 from xml.dom.minidom import parseString
 
 app = Flask(__name__)
-auth = HTTPBasicAuth
+auth = HTTPBasicAuth()
 
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
@@ -15,6 +15,35 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
+@auth.verify_password
+def verify_password(username, password):
+    return username == "ron" and password == "110"
+
+def convert_to_xml(data):
+    xml = dicttoxml.dicttoxml(data, custom_root='response', attr_type=False)
+    dom = parseString(xml)
+    return dom.toprettyxml()
+
+def format_response(data):
+    response_format = request.args.get('format', 'json').lower()
+    if response_format == 'xml':
+        xml_data = convert_to_xml(data)
+        return make_response(xml_data, 200, {'Content-Type': 'application/xml'})
+    else:
+        return make_response(jsonify(data), 200)
+    
+@app.route("/protected")
+@auth.login_required
+def protected_resource():
+    return jsonify({"message": "You are authorized to acces this resource."})
+
+
+@app.route("/")
+@auth.login_required
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
 def data_fetch(query):
     cur - mysql.connection.cursor()
     cur.execute(query)
@@ -22,9 +51,7 @@ def data_fetch(query):
     cur.close()
     return data
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
